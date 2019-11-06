3122d7b8f2f94ba9b5db4b9ea5edee326d3284cd
Imanol Bracero <imanol.bracero@touchsurgery.com>
2019-11-06 19:06:11 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+import BuildTypes.DEBUG
+import BuildTypes.RELEASE
+
+plugins{
+    id(Plugins.androidApplication)
+    id(Plugins.kotlinAndroid)
+    id(Plugins.kotlinAndroidExtensions)
+}
+
+android {
+    compileSdkVersion(AndroidSdk.compile)
+    defaultConfig {
+        applicationId = "com.ibracero.retrum"
+        minSdkVersion(AndroidSdk.min)
+        targetSdkVersion(AndroidSdk.target)
+        versionCode = Project.versionCode
+        versionName = Project.versionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName(DEBUG) {
+            isMinifyEnabled = false
+            isDebuggable = true
+        }
+
+        getByName(RELEASE) {
+            isMinifyEnabled = true
+            isDebuggable = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(Libraries.kotlin_std)
+    implementation(Libraries.androidx_appcompat)
+    implementation(Libraries.androidx_core_ktx)
+    implementation(Libraries.androidx_material)
+    implementation(Libraries.androidx_constraint_layout)
+    implementation(Libraries.androidx_lifecycle_extensions)
+    implementation(Libraries.androidx_navigation_fragment)
+    implementation(Libraries.androidx_navigation_fragment_ktx)
+    implementation(Libraries.androidx_navigation_ui)
+    implementation(Libraries.androidx_navigation_ui_ktx)
+
+    testImplementation(TestLibraries.junit)
+    androidTestImplementation(TestLibraries.androidx_test_espresso_core)
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,60 @@
+import Libraries.Versions.androidx_compat_version
+import Libraries.Versions.androidx_constraint_layout_version
+import Libraries.Versions.androidx_core_version
+import Libraries.Versions.androidx_lifecycle_version
+import Libraries.Versions.androidx_version
+import Libraries.Versions.google_material_version
+import TestLibraries.Versions.espresso_version
+import TestLibraries.Versions.junit_version
+
+const val kotlin_version = "1.3.50"
+
+object AndroidSdk {
+    const val min = 21
+    const val compile = 29
+    const val target = compile
+}
+
+object Project{
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
+
+object Plugins{
+    const val androidApplication = "com.android.application"
+    const val kotlinAndroid = "kotlin-android"
+    const val kotlinAndroidExtensions = "kotlin-android-extensions"
+}
+
+object Libraries {
+    object Versions {
+        const val androidx_version = "2.0.0"
+        const val androidx_compat_version = "1.0.2"
+        const val androidx_core_version = "1.0.2"
+        const val androidx_constraint_layout_version = "1.1.3"
+        const val androidx_lifecycle_version = "2.0.0"
+        const val google_material_version = "1.0.0"
+    }
+
+    const val kotlin_std = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version"
+
+    const val androidx_appcompat = "androidx.appcompat:appcompat:$androidx_compat_version"
+    const val androidx_core_ktx = "androidx.core:core-ktx:$androidx_core_version"
+    const val androidx_material = "com.google.android.material:material:$google_material_version"
+    const val androidx_lifecycle_extensions = "androidx.lifecycle:lifecycle-extensions:$androidx_lifecycle_version"
+    const val androidx_constraint_layout = "androidx.constraintlayout:constraintlayout:$androidx_constraint_layout_version"
+    const val androidx_navigation_fragment = "androidx.navigation:navigation-fragment:$androidx_version"
+    const val androidx_navigation_fragment_ktx = "androidx.navigation:navigation-fragment-ktx:$androidx_version"
+    const val androidx_navigation_ui = "androidx.navigation:navigation-ui:$androidx_version"
+    const val androidx_navigation_ui_ktx = "androidx.navigation:navigation-ui-ktx:$androidx_version"
+}
+
+object TestLibraries {
+    private object Versions {
+        const val junit_version = "4.12"
+        const val espresso_version = "3.1.1"
+    }
+
+    const val junit = "junit:junit:$junit_version"
+    const val androidx_test_espresso_core = "androidx.test.espresso:espresso-core:$espresso_version"
+}
